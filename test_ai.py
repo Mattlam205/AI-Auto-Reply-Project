888d4e3b60071e6a3c2ce3a926266b5e81cfbc9c
Mattlam205 <mclmc205@gmail.com>
2026-05-21 00:49:36 -0700

    First commit for this proj

---
+++ test_ai.py
@@ -0,0 +1,20 @@
+import ollama
+
+while True:
+
+    user_input = input("You: ")
+
+    if user_input.lower() == "exit":
+        break
+
+    response = ollama.chat(
+        model='llama3',
+        messages=[
+            {
+                'role': 'user',
+                'content': user_input
+            }
+        ]
+    )
+
+    print("AI:", response['message']['content'])
