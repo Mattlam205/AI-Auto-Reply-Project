faff30acdb85a0317a11e6744b67073704c7dc7a
Mattlam205 <mclmc205@gmail.com>
2026-05-22 21:49:50 -0700

    Update AI chatgbot UI and succesfully have conversation with Ollama AI

--- app.py
+++ app.py
@@ -1,9 +1,27 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
+import ollama
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods =["GET", "POST"])
+
 def home():
-    return render_template("index.html")
+    ai_response = ""
+    user_message = ""
+
+    if request.method == "POST":
+        user_message = request.form["message"]
+
+        if user_message.strip() != "":
+            response = ollama.chat(
+             model = "llama3",
+             messages =[
+                {"role": "user", "content" : user_message}
+                ]
+        )
+
+        ai_response = response["message"]["content"]
+
+    return render_template("index.html", user_message = user_message , ai_response = ai_response)
 
 app.run(debug=True)
